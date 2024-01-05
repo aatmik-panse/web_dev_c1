f9fe9dc60f40946cdd29bbb4dcd4ac41333ae47a
aatmik-panse <140905599+aatmik-panse@users.noreply.github.com>
2024-01-05 15:05:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("scroll", function () {
-        var navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+    var navbar = document.getElementById("navbar");
 
-        if (window.scrollY > 50) {
-            navbar.style.backgroundColor = "#3498db";
-        } else {
-            navbar.style.backgroundColor = "transparent";
-        }
-    });
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = "#3498db";
+    } else {
+        navbar.style.backgroundColor = "transparent";
+    }
 });
