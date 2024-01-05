278a45bda1d8f608b717afbdd065899199518543
aatmik-panse <140905599+aatmik-panse@users.noreply.github.com>
2024-01-05 14:12:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 0) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        var navbar = document.getElementById("navbar");
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-document.addEventListener('DOMContentLoaded', (event) => {
-    const backToTopButton = document.getElementById('back-to-top');
-
-    window.onscroll = function() {
-        if (window.pageYOffset > 100) { // If the scroll is more than 100px
-            backToTopButton.style.display = "block";
+        if (window.scrollY > 50) {
+            navbar.style.backgroundColor = "#3498db";
         } else {
-            backToTopButton.style.display = "none";
+            navbar.style.backgroundColor = "transparent";
         }
-    };
-
-    backToTopButton.onclick = function() {
-        window.scrollTo({top: 0, behavior: 'smooth'}); // Smooth scroll to top
-    };
+    });
 });
